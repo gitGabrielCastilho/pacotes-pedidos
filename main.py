a69ca1c200251e8e40897285c262e6f03c23b3f5
Gabriel <gabrielmcastilho@gmail.com>
2023-07-05 15:36:49 -0300

    começando expressão

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ dst_path = r'MTK:C:/Microsys/MsysIndustrial/Dados/MSYSDADOS.FDB'
 TABLE_NAME1 = 'PEDIDOS_VENDAS'
 
 ########################################################################
-SELECT1 = 'select PDV_CLI_CODIGO, PDV_NUMERO, PDV_TIPOPAGAMENTO, PDV_VALORPRODUTOS from %s ' \
+SELECT1 = 'select PDV_CLI_CODIGO, PDV_NUMERO, PDV_CON_CODIGO, PDV_VALORPRODUTOS from %s ' \
           % (TABLE_NAME1)
 ########################################################################
 con = fdb.connect(dsn=dst_path, user='SYSDBA', password='masterkey', charset='UTF8')
@@ -16,18 +16,21 @@ cur.execute(SELECT1)
 table_rows1 = cur.fetchall()
 ########################################################################
 df1 = pd.DataFrame(table_rows1)
-df1 = df1[df1[2] == "B"]
+#df1 = df1[df1[2] == "B"]
 ########################################################################
 print("Digite o cód do cliente: ")
-x = int(input())
-df1 = df1[df1[0] == x]
+cod_cli = int(input())
+df1 = df1[df1[0] == cod_cli]
 ########################################################################
 print("Digite o cód do pedido de pacote: ")
-y = int(input())
-df2 = df1.loc[df1[1] == y]
+num_pedido = int(input())
+df2 = df1.loc[df1[1] == num_pedido]
+df2 = df2.reset_index(drop=True)
+valor_pedido = df2.iloc[0,3]
+########################################################################
+df1 = df1.reset_index(drop=True)
+total = df1[3].sum()
+########################################################################
 
-df2 = df2.rename(index = lambda x: 0)
-df2 = df2.rename(index =lambda x: x+1)
-
-
-print(df2.head(15))
+#print(total_atual)
+#print(df1)
