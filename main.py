d4d210e0030f302e6989028a6a2551029ecd6200
Gabriel <gabrielmcastilho@gmail.com>
2023-07-06 09:40:13 -0300

    adicionando yale/tetra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import fdb
+from tkinter import *
 
 dst_path = r'MTK:C:/Microsys/MsysIndustrial/Dados/MSYSDADOS.FDB'
 
@@ -21,23 +22,50 @@ print("Digite o cód do cliente: ")
 cod_cli = int(input())
 df1 = df1[df1[0] == cod_cli]
 ########################################################################
-print("Digite o cód do pedido de pacote: ")
-num_pedido = int(input())
-df2 = df1.loc[df1[1] == num_pedido]
+print("Digite o cód do pedido de pacote de Yale: ")
+num_pedido_yale = int(input())
+df2 = df1.loc[df1[1] == num_pedido_yale]
 df2 = df2.reset_index(drop=True)
-valor_pedido = df2.iloc[0,3]
+valor_pedido_yale = df2.iloc[0,3]
+
 ########################################################################
-df1 = df1[df1[2] == 3]
-df1 = df1.reset_index(drop=True)
-total = df1[3].sum()
+dfx = df1[df1[2] == 3]
+dfx = dfx.reset_index(drop=True)
+total = dfx[3].sum()
 ########################################################################
-valor_restante = valor_pedido - total
+valor_restante_yale = valor_pedido_yale - total
 
-if valor_restante > 0:
-    print(f"O cliente tem o valor de R${valor_restante:,.2f} para retirar do pacote {num_pedido}")
 
-elif valor_restante < 0:
-    print(f"O pacote {num_pedido} do cliente acabou, ele retirou R${valor_restante:,.2f} a mais do pacote")
+if valor_restante_yale > 0:
+    print(f"O cliente tem o valor de R${valor_restante_yale:,.2f} para retirar do pacote {num_pedido_yale}")
 
-elif valor_restante == 0:
-    print(f"O cliente completou a retirada do pacote {num_pedido}")
+elif valor_restante_yale < 0:
+    print(f"O pacote {num_pedido_yale} do cliente acabou, ele retirou R${valor_restante_yale:,.2f} a mais do pacote")
+
+elif valor_restante_yale == 0:
+    print(f"O cliente completou a retirada do pacote {num_pedido_yale}")
+
+print(50*"#")
+#########################################################################
+print("Digite o cód do pedido de pacote de Tetra, caso não tenha tetra, digite 0: ")
+
+num_pedido_tetra = int(input())
+
+if num_pedido_tetra == 0:
+    pass
+else:
+    df3 = df1.loc[df1[1] == num_pedido_tetra]
+    df3 = df3.reset_index(drop=True)
+    valor_pedido_tetra = df3.iloc[0,3]
+    valor_restante_tetra = valor_pedido_tetra - total
+
+    if valor_restante_tetra > 0:
+        print(f"O cliente tem o valor de R${valor_restante_tetra:,.2f} para retirar do pacote {num_pedido_tetra}")
+
+    elif valor_restante_tetra < 0:
+        print(
+            f"O pacote {num_pedido_tetra} do cliente acabou, ele retirou R${valor_restante_tetra:,.2f} a mais do pacote")
+
+    elif valor_restante_tetra == 0:
+        print(f"O cliente completou a retirada do pacote {num_pedido_tetra}")
+
