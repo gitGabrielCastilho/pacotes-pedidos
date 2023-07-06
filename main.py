80b5d730bb875f6aa5d89faac41328ad7304cf6f
Gabriel <gabrielmcastilho@gmail.com>
2023-07-06 17:01:41 -0300

    interface finalizada

--- main.py
+++ main.py
@@ -1,71 +1,129 @@
 import pandas as pd
 import fdb
+import tkinter
 from tkinter import *
 
-dst_path = r'MTK:C:/Microsys/MsysIndustrial/Dados/MSYSDADOS.FDB'
+def enter_data():
+    dst_path = r'MTK:C:/Microsys/MsysIndustrial/Dados/MSYSDADOS.FDB'
 
-TABLE_NAME1 = 'PEDIDOS_VENDAS'
-
-########################################################################
-SELECT1 = 'select PDV_CLI_CODIGO, PDV_NUMERO, PDV_CON_CODIGO, PDV_VALORPRODUTOS from %s ' \
-          % (TABLE_NAME1)
-########################################################################
-con = fdb.connect(dsn=dst_path, user='SYSDBA', password='masterkey', charset='UTF8')
-cur = con.cursor()
-########################################################################
-cur.execute(SELECT1)
-table_rows1 = cur.fetchall()
-########################################################################
-df1 = pd.DataFrame(table_rows1)
-########################################################################
-print("Digite o cód do cliente: ")
-cod_cli = int(input())
-df1 = df1[df1[0] == cod_cli]
-########################################################################
-print("Digite o cód do pedido de pacote de Yale: ")
-num_pedido_yale = int(input())
-df2 = df1.loc[df1[1] == num_pedido_yale]
-df2 = df2.reset_index(drop=True)
-valor_pedido_yale = df2.iloc[0,3]
+    TABLE_NAME1 = 'PEDIDOS_VENDAS'
 
 ########################################################################
-dfx = df1[df1[2] == 3]
-dfx = dfx.reset_index(drop=True)
-total = dfx[3].sum()
+    SELECT1 = 'select PDV_CLI_CODIGO, PDV_NUMERO, PDV_CON_CODIGO, PDV_VALORPRODUTOS from %s ' \
+            % (TABLE_NAME1)
 ########################################################################
-valor_restante_yale = valor_pedido_yale - total
+    con = fdb.connect(dsn=dst_path, user='SYSDBA', password='masterkey', charset='UTF8')
+    cur = con.cursor()
+########################################################################
+    cur.execute(SELECT1)
+    table_rows1 = cur.fetchall()
+########################################################################
+    df1 = pd.DataFrame(table_rows1)
+########################################################################
+
+    cod_cli = cod_cli_entry.get()
+    cod_cli = int(cod_cli)
+    df1 = df1[df1[0] == cod_cli]
+
+# ########################################################################
+    num_pedido_yale = yale_entry.get()
+    num_pedido_yale = int(num_pedido_yale)
+    df2 = df1.loc[df1[1] == num_pedido_yale]
+    df2 = df2.reset_index(drop=True)
+    valor_pedido_yale = df2.iloc[0,3]
+# ########################################################################
+    dfx = df1[df1[2] == 3]
+    dfx = dfx.reset_index(drop=True)
+    total = dfx[3].sum()
+# ########################################################################
+
+    valor_restante_yale = (valor_pedido_yale - total)
+
+    if valor_restante_yale > 0:
+        var_yale.set(f"O cliente tem o valor de {valor_restante_yale:,.2f} para retirar do pacote {num_pedido_yale}")
+
+    elif valor_restante_yale < 0:
+        var_yale.set(f"O pacote {num_pedido_yale} do cliente acabou, retirou R${valor_restante_yale:,.2f} a mais do pacote")
+
+    elif valor_restante_yale == 0:
+        var_yale.set(f"O cliente completou a retirada do pacote {num_pedido_yale}")
 
 
-if valor_restante_yale > 0:
-    print(f"O cliente tem o valor de R${valor_restante_yale:,.2f} para retirar do pacote {num_pedido_yale}")
+##########################################################################
+    num_pedido_tetra = tetra_entry.get()
+    num_pedido_tetra= int(num_pedido_tetra)
 
-elif valor_restante_yale < 0:
-    print(f"O pacote {num_pedido_yale} do cliente acabou, ele retirou R${valor_restante_yale:,.2f} a mais do pacote")
+    if num_pedido_tetra == 0:
+        pass
+    elif num_pedido_tetra != 0:
+        dfy = df1[df1[2] == 3]
+        dfy = dfy.reset_index(drop=True)
+        total_tetra = dfy[3].sum()
 
-elif valor_restante_yale == 0:
-    print(f"O cliente completou a retirada do pacote {num_pedido_yale}")
+        df3 = df1.loc[df1[1] == num_pedido_tetra]
+        df3 = df3.reset_index(drop=True)
+        valor_pedido_tetra = df3.iloc[0,3]
+        valor_restante_tetra = valor_pedido_tetra - total_tetra
 
-print(50*"#")
-#########################################################################
-print("Digite o cód do pedido de pacote de Tetra, caso não tenha tetra, digite 0: ")
+        if valor_restante_tetra == 0:
+            var_tetra.set(f"O cliente completou a retirada do pacote {num_pedido_tetra}")
 
-num_pedido_tetra = int(input())
+        elif valor_restante_tetra > 0:
+            var_tetra.set(f"O cliente tem o valor de R${valor_restante_tetra:,.2f} para retirar do pacote {num_pedido_tetra}")
 
-if num_pedido_tetra == 0:
-    pass
-else:
-    df3 = df1.loc[df1[1] == num_pedido_tetra]
-    df3 = df3.reset_index(drop=True)
-    valor_pedido_tetra = df3.iloc[0,3]
-    valor_restante_tetra = valor_pedido_tetra - total
+        elif valor_restante_tetra < 0:
+            var_tetra.set(f"O pacote {num_pedido_tetra} do cliente acabou, ele retirou R${valor_restante_tetra:,.2f} a mais do pacote")
 
-    if valor_restante_tetra > 0:
-        print(f"O cliente tem o valor de R${valor_restante_tetra:,.2f} para retirar do pacote {num_pedido_tetra}")
 
-    elif valor_restante_tetra < 0:
-        print(
-            f"O pacote {num_pedido_tetra} do cliente acabou, ele retirou R${valor_restante_tetra:,.2f} a mais do pacote")
 
-    elif valor_restante_tetra == 0:
-        print(f"O cliente completou a retirada do pacote {num_pedido_tetra}")
+window = tkinter.Tk()
+window.title('Calculadora de Pacotes')
 
+frame = tkinter.Frame(window)
+frame.pack()
+
+user_info_frame = tkinter.LabelFrame(frame, text='Cliente')
+user_info_frame.grid(row=0 , column = 0, padx=10, pady=10, sticky="news")
+
+cod_cli_label = tkinter.Label(user_info_frame, text ='Código cliente: ')
+cod_cli_label.grid(row=0, column=0, pady=5)
+cod_cli_entry = tkinter.Entry(user_info_frame)
+cod_cli_entry.grid(row=1, column=0, padx=5, pady=5)
+##################################################################################
+yales_frame = tkinter.LabelFrame(frame, text="Pedido Yale")
+yales_frame.grid(row=1, column=0, padx=10, pady=10, sticky="news")
+
+yale_label = tkinter.Label(yales_frame, text="Pedido Yale:")
+yale_label.grid(row=0, column=0, pady = 5)
+yale_entry = tkinter.Entry(yales_frame)
+yale_entry.grid(row=1, column=0, padx=5, pady=5)
+##################################################################################
+tetra_frame = tkinter.LabelFrame(frame, text="Pedido Outras Chaves")
+tetra_frame.grid(row=2, column = 0, padx=10, pady=10, sticky="news")
+tetra_label = tkinter.Label(tetra_frame, text="Pedido outras chaves:")
+tetra_label_info = tkinter.Label(tetra_frame, text="Digite 0 caso não houver ")
+tetra_label.grid(row=0, column=0, pady=5)
+tetra_label_info.grid(row=1, column=0)
+tetra_entry = tkinter.Entry(tetra_frame)
+tetra_entry.grid(row=2, column=0, pady=5)
+
+
+##################################################################################
+button = tkinter.Button(frame, text="Confirmar", command=enter_data)
+button.grid(row=3, column=0, sticky="news", pady=5, padx=10)
+
+##################################################################################
+resultado_frame = tkinter.LabelFrame(frame, text="Resultado")
+resultado_frame.grid(row=4, column=0, padx=10, pady=10, sticky="news")
+
+var_yale = StringVar()
+var_yale.set("a")
+
+resultado_yale = tkinter.Label(resultado_frame, textvariable = var_yale)
+resultado_yale.grid(row=0, column=0, padx=10, pady=10)
+
+var_tetra = StringVar()
+var_tetra.set("")
+resultado_tetra = tkinter.Label(resultado_frame, textvariable = var_tetra)
+resultado_tetra.grid(row=0, column=0, padx=10, pady=10)
+window.mainloop()
